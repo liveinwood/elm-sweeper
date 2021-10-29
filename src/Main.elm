4a3211caba7f0fdfb487b0716293d5565bfbede8
liveinwood <liveinwood@gmail.com>
2021-10-29 09:35:25 +0900

    WIP

--- src/Main.elm
+++ src/Main.elm
@@ -1,8 +1,10 @@
 module Main exposing (..)
 
-import List exposing (concatMap, map)
-import Matrix exposing (Matrix, empty, fromLists)
-import Maybe exposing (withDefault)
+import Array exposing (Array)
+import List.Extra as LE
+import Matrix as M exposing (Matrix)
+import Random as R exposing (Generator)
+import Random.Array as RA
 
 
 
@@ -14,39 +16,50 @@ width =
     5
 
 
+randomBoolGenerator : Generator Bool
+randomBoolGenerator =
+    R.map (\i -> i == 0) (R.int 0 1)
+
+
+random2DBoolGenerator : Generator (Array (Array Bool))
+random2DBoolGenerator =
+    RA.array width (RA.array width randomBoolGenerator)
+
+
+get : Array (Array Bool) -> ( Int, Int ) -> Bool
+get array2d ( x, y ) =
+    Array.get x array2d |> Maybe.andThen (Array.get y) |> Maybe.withDefault False
+
+
+neighbours : ( Int, Int ) -> List ( Int, Int )
+neighbours ( x, y ) =
+    [ -1, 0, 1 ]
+        |> LE.andThen
+            (\a ->
+                [ -1, 0, 1 ]
+                    |> LE.andThen
+                        (\b ->
+                            if a == 0 && b == 0 then
+                                []
+
+                            else
+                                [ ( x + a, y + b ) ]
+                        )
+            )
+
+
+aroundFalseCount : ( Int, Int ) -> Array (Array Bool) -> Int
+aroundFalseCount ( x, y ) array2d =
+    neighbours ( x, y ) |> List.map (get array2d) |> List.filter (\b -> b == False) |> List.length
+
+
+initialize : Array (Array Bool) -> Matrix Cell
+initialize array2d =
+    M.initialize width width (\x y -> Cell False (get array2d ( x, y )) (aroundFalseCount ( x, y ) array2d))
+
+
 type alias Cell =
     { opened : Bool
     , safe : Bool
     , count : Int
     }
-
-
-initialModel : Matrix Bool
-initialModel =
-    withDefault empty (fromLists [ [ True, False, True ], [ False, True, True ], [ False, False, True ] ])
-
-
-{-| 隣接するセルの座標を返す
-
-    neighbors ( 0, 0 ) =
-        [ ( -1, -1 ), ( -1, 0 ), ( -1, 1 ), ( 0, -1 ), ( 0, 1 ), ( 1, -1 ), ( 1, 0 ), ( 1, 1 ) ]
-    neighbors ( 1, 1 ) =
-        [ ( 0, 0 ), ( 0, 1 ), ( 0, 2 ), ( 1, 0 ), ( 1, 2 ), ( 2, 0 ), ( 2, 1 ), ( 2, 2 ) ]
-
--}
-neighbors : ( Int, Int ) -> List ( Int, Int )
-neighbors ( r, c ) =
-    [ -1, 0, 1 ]
-        |> concatMap
-            (\x ->
-                [ -1, 0, 1 ]
-                    |> concatMap
-                        (\y ->
-                            if x /= 0 || y /= 0 then
-                                [ ( x, y ) ]
-
-                            else
-                                []
-                        )
-            )
-        |> map (\( x, y ) -> ( r + x, c + y ))
