feb7a031f4858ecec3749f1e9e6b22817928b4cc
liveinwood <liveinwood@gmail.com>
2021-11-02 00:30:54 +0900

    WIP

--- src/Main.elm
+++ src/Main.elm
@@ -1,6 +1,7 @@
 module Main exposing (..)
 
 import Array exposing (Array)
+import Html exposing (a)
 import List.Extra as LE
 import Matrix as M exposing (Matrix)
 import Random as R exposing (Generator)
@@ -26,21 +27,31 @@ random2DBoolGenerator =
     RA.array width (RA.array width randomBoolGenerator)
 
 
-get : Array (Array Bool) -> ( Int, Int ) -> Bool
+getWithDefault : a -> Array (Array a) -> ( Int, Int ) -> a
+getWithDefault d array2d ( x, y ) =
+    get array2d ( x, y ) |> Maybe.withDefault d
+
+
+get : Array (Array a) -> ( Int, Int ) -> Maybe a
 get array2d ( x, y ) =
-    Array.get x array2d |> Maybe.andThen (Array.get y) |> Maybe.withDefault False
+    Array.get x array2d |> Maybe.andThen (Array.get y)
 
 
-neighbours : ( Int, Int ) -> List ( Int, Int )
-neighbours ( x, y ) =
+neighbours : ( Int, Int ) -> Array (Array Bool) -> List Bool
+neighbours ( x, y ) array2d =
     [ -1, 0, 1 ]
         |> LE.andThen
             (\a ->
                 [ -1, 0, 1 ]
                     |> LE.andThen
                         (\b ->
-                            if x + a >= 0 && x + a <= width && y + b >= 0 && y + b <= width then
-                                [ ( x + a, y + b ) ]
+                            if a /= 0 || b /= 0 then
+                                case get array2d ( x + a, y + b ) of
+                                    Just z ->
+                                        [ z ]
+
+                                    Nothing ->
+                                        []
 
                             else
                                 []
@@ -50,12 +61,19 @@ neighbours ( x, y ) =
 
 aroundFalseCount : ( Int, Int ) -> Array (Array Bool) -> Int
 aroundFalseCount ( x, y ) array2d =
-    neighbours ( x, y ) |> List.map (get array2d) |> List.filter (\b -> not b) |> List.length
+    neighbours ( x, y ) array2d |> List.filter (\b -> not b) |> List.length
 
 
 initialize : Array (Array Bool) -> Matrix Cell
 initialize array2d =
-    M.initialize width width (\x y -> Cell False (get array2d ( x, y )) (aroundFalseCount ( x, y ) array2d))
+    let
+        h =
+            Array.length array2d
+
+        w =
+            Array.length (Maybe.withDefault Array.empty (Array.get 0 array2d))
+    in
+    M.initialize h w (\x y -> Cell False (getWithDefault False array2d ( x, y )) (aroundFalseCount ( x, y ) array2d))
 
 
 type alias Cell =
